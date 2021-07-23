8029479fead236b92394d7112e19859da99a369f
ZyadHany35 <86461552+ZyadHany35@users.noreply.github.com>
2021-07-23 18:51:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+let pass = document.getElementById("pass");
+let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"; /* 61 charcter */
+let charCount = 2;
+let timer = document.getElementById("timer");
+let timerTime = 0;
+let wait = 1000;
+let restartBtn = document.getElementById("btnT")
+let searching = window.prompt("Write Two Charcters!");
+
+let i = 0;
+let j = 0;
+if (searching.length > 2) {
+    location.reload()
+}
+setInterval(() => {
+    i++
+}, characters.length * wait);
+
+setInterval(() => {
+    timerTime += wait / 1000;
+    timer.innerHTML = `${timerTime.toFixed(2)}s`;
+    if (j < characters.length - 1) { 
+        j++
+    } else {
+        j = 0;
+    }
+        pass.innerHTML = `${characters[i]}${characters[j]}`
+        if (pass.innerHTML == searching || pass.innerHTML == "Zero If You Want" ) {
+            document.write(`<div class="pass" id="fin">Finished => You Got "${characters[i]}${characters[j]}" In ${timerTime.toFixed(2)}s</div>`)
+            let fin = document.getElementById("fin")
+            fin.style.fontSize = "50px";
+            fin.style.padding = "20px";
+            fin.style.backgroundColor = "chartreuse";
+            fin.style.borderRadius = "6px";
+            fin.style.textAlign = "center";
+        }
+
+}, wait);
